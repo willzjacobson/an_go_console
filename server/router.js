49354ca2cfbff606740356cc82b10aaf9e22f258
William Jacobson <williamjacobson@Williams-MacBook-Pro.local>
2016-04-12 02:58:47 -0400

    yup

--- server/router.js
+++ server/router.js
@@ -1,5 +1,6 @@
 var router = require('express').Router();
 var dbProm = require('./mongo');
+var mongodb = require('mongodb');
 
 
 router.get('/', function(req, res, next) {
@@ -16,31 +17,27 @@ router.post('/', function(req, res, next) {
   dbProm.then(function(db) {
   	db.collection('buildings').insert(req.body, function(err, result) {
   		if (err) throw err;
-  		console.log(result);
   		res.json(result.ops[0]);
-  	})
+  	});
   })
   .then(null, next);
 });
 
+// Couldn't get update to work, so deleting then inserting new doc (_id will change)
 router.put('/:buildingId', function(req, res, next) {
 	dbProm.then(function(db) {
-		db.collection('buildings').update({_id: req.params.buildingId}, req.body);
-		// db.collection('buildings').update({_id: req.params.buildingId}, req.body, {new:true, w:1}, function(err, result) {
-		// 	if (err) throw err;
-		// 	console.log(result);
-		// 	res.json(result);
-		// });
-		res.end()
+		db.collection('buildings').update({_id: new mongodb.ObjectID(req.params.buildingId)}, req.body, {new:true, w:1}, function(err, result) {
+			if (err) throw err;
+			res.json(result);
+		});
 	});
 });
 
 router.delete('/:buildingId', function(req, res, next) {
 	dbProm.then(function(db) {
-		db.collection('buildings').deleteOne({_id:req.params.id}, {remove:true}, function(err, object) {
+		db.collection('buildings').deleteOne({_id: new mongodb.ObjectID(req.params.buildingId)}, function(err, object) {
 			if (err) throw err;
-			console.log('document deleted');
-			res.end();
+			res.send('Doc deleted');
 		});
 	})
 	.then(null, next);
